76f88c75ded0eb5aa6e2d9f10d2d8383c43f432c
Chi-Ming Yip <cmyip99@gmail.com>
2020-09-25 22:11:52 +1000

    Added firebase config

--- App.tsx
+++ App.tsx
@@ -6,6 +6,22 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAXUmsXC1r1mg5uuh7G5jamafcWul9YRSM",
+  authDomain: "mediplus-1f427.firebaseapp.com",
+  databaseURL: "https://mediplus-1f427.firebaseio.com",
+  projectId: "mediplus-1f427",
+  storageBucket: "mediplus-1f427.appspot.com",
+  messagingSenderId: "1065801771983",
+  appId: "1:1065801771983:web:c0b4d908a9e67ee5bac0ba",
+  measurementId: "G-BVEF0GB2R2"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
