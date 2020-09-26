5f0dd36e33066206140051de8a38ea15a932867c
Timothy Samraj <tsam0005@student.monash.edu>
2020-09-26 20:51:49 +1000

    Update calendar

--- screens/Calendar.tsx
+++ screens/Calendar.tsx
@@ -1,36 +1,31 @@
 import React, { Component } from "react";
-import { StyleSheet, View, StatusBar, Text, Image } from "react-native";
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { StyleSheet, View, StatusBar, Text, Image, Dimensions } from "react-native";
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 function Explore() {
   return (
     <View style={styles.container}>
       <StatusBar hidden />
       <Text style={styles.dateTime}>Date &amp; Time</Text>
-
       <Image
         source={require("../assets/images/CalendarTop.png")}
         resizeMode="contain"
         style={styles.image}
       ></Image>
-
-      <View style = {styles.calendar}>
-      <CalendarList
-        pastScrollRange={24}
-        futureScrollRange={24}
-        horizontal
-        pagingEnabled
+      <View style={styles.calendar}>
+        <CalendarList
+          pastScrollRange={24}
+          futureScrollRange={24}
+          horizontal
+          pagingEnabled
         />
       </View>
-
       <Text style={styles.text74}>Select from the available times:</Text>
-
       <Image
         source={require("../assets/images/calendartimes.png")}
         resizeMode="contain"
         style={styles.image2}
       ></Image>
-
       <Image
         source={require("../assets/images/saveButtonCalendar.png")}
         resizeMode="contain"
@@ -43,48 +38,53 @@ function Explore() {
 
 const styles = StyleSheet.create({
   container: {
+    alignContent: "center",
     flex: 1,
     backgroundColor: "rgba(249,249,249,1)"
   },
 
-  calendar: { 
-      top: 50
+  calendar: {
+    top: -35
   },
 
   dateTime: {
     fontFamily: "Lato_400Regular",
     color: "rgba(23,34,133,1)",
     fontSize: 35,
-    marginTop: 78,
+    marginTop: 20,
     marginLeft: 34
   },
   text74: {
     fontFamily: "Lato_400Regular",
     color: "rgba(161,164,178,1)",
-    marginTop: 328,
+    marginTop: -500,
     marginLeft: 34
   },
   image: {
-    top: 10
+    top: 10,
+    alignSelf: "center",
+    width: Dimensions.get('window').width
   },
   image2: {
+    alignSelf: "center",
     height: 123,
     width: 295,
     marginTop: 291,
     marginLeft: 32
   },
   image3: {
+    alignSelf: "center",
     height: 71,
     width: 278,
     marginTop: 8,
-    marginLeft: 44
+    // marginLeft: 44
   },
   noThanks: {
+    alignSelf: "center",
     fontFamily: "Lato_400Regular",
     color: "rgba(161,164,178,1)",
     fontSize: 10,
     marginTop: 5,
-    marginLeft: 156
   }
 });
 
