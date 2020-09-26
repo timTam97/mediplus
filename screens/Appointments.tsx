9de332fe4374ca379d1f8bcfc98087baba6f20fc
Timothy Samraj <tsam0005@student.monash.edu>
2020-09-26 17:11:04 +1000

    Depart messages DONE

--- screens/Appointments.tsx
+++ screens/Appointments.tsx
@@ -5,9 +5,10 @@ import MapView from 'react-native-maps';
 import * as DirectionUtil from '../components/MyDirectionAdapter'
 import * as Location from 'expo-location';
 import { Text, View } from 'react-native';
+import { setLogLevel } from 'firebase';
 
 var appointmentTime: number = Date.now();
-appointmentTime = appointmentTime + (14 * (60 * 60 * 1000))
+appointmentTime = appointmentTime + (48 * (60 * 60 * 1000))
 
 class UpdateTime extends React.Component {
     componentDidMount() {
@@ -35,7 +36,7 @@ class UpdateCountdown extends React.Component {
         setInterval(
             () => {
                 this.setState({
-                    message: "You have " + Math.ceil(Math.ceil(Math.abs(appointmentTime - Date.now())) / (1000 * 60 * 60)) + " minutes until your\nappointment at South Yarra Clinic"
+                    message: "You have " + Math.ceil(Math.ceil(Math.abs(appointmentTime - Date.now())) / (1000 * 60 * 60)) + " minutes until your\nappointment at South Yarra Clinic."
                 }); /*console.log(appointmentTime - Date.now() / (1000 * 60))*/
             },
             1000
@@ -50,6 +51,13 @@ class UpdateCountdown extends React.Component {
     }
 }
 
+type departState = {
+    counter: number,
+    timeToLeave: number,
+    timeToLeaveSecs: number,
+    message: string
+}
+
 class UpdateDepartureTime extends React.Component {
     componentDidMount() {
         // in the end we gon update every minute.
@@ -57,16 +65,25 @@ class UpdateDepartureTime extends React.Component {
         // for snooping and updating location-based stuff
         setInterval(
             () => {
-                this.setState({
-                    message: "Gonna take you " + DirectionUtil.getTravel() + " minutes i think"
-                }); console.log("here " + DirectionUtil.getTravel())
+                // console.log(this.state.counter)
+                let newCounter = this.state.counter + 1
+                let newTime: number = (this.state.counter % 60 == 0) ? this.state.timeToLeave - 1 : this.state.timeToLeave;
+                let newSecs = (this.state.timeToLeaveSecs == 0) ? 59 : this.state.timeToLeaveSecs - 1
+                this.setState(previousState => ({
+                    counter: newCounter,
+                    timeToLeave: newTime,
+                    timeToLeaveSecs: newSecs,
+                    message: "You should leave in " + newTime + " minutes and " + newSecs + " seconds to arrive on time."
+                }))
             },
-            10000
+            1000
         );
     }
-    state = {
-        // message: "You have " + Math.ceil(Math.ceil(Math.abs(Date.now() - appointmentTime)) / (1000 * 60 * 60)) + " minutes until your appointment at South Yarra Clinic"
-        message: "Gonna take you " + DirectionUtil.getTravel() + " minutes i think"
+    state: departState = {
+        counter: 0,
+        timeToLeave: 27,
+        timeToLeaveSecs: 59,
+        message: "You should leave in 27 minutes and 59 seconds to arrive on time."
     }
     render() {
         return <Text style={styles.departure}>{this.state.message}</Text>;
@@ -127,9 +144,11 @@ const styles = StyleSheet.create({
         marginLeft: 25
     },
     departure: {
+        fontFamily: "Lato_400Regular",
+        fontSize: 24,
         height: 56,
-        width: 172,
-        marginTop: 350,
+        width: 300,
+        marginTop: 325,
         marginLeft: 25
     }
 });
