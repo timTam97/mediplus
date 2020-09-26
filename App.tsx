dad944a06ffe515d34ebf8f2541ba4f9bafe4a77
Timothy Samraj <tsam0005@student.monash.edu>
2020-09-27 01:24:06 +1000

    Bye bye firebase

--- App.tsx
+++ App.tsx
@@ -21,24 +21,6 @@ import {
   Lato_900Black,
   Lato_900Black_Italic,
 } from '@expo-google-fonts/lato';
-import firebase from 'firebase';
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAXUmsXC1r1mg5uuh7G5jamafcWul9YRSM",
-  authDomain: "mediplus-1f427.firebaseapp.com",
-  databaseURL: "https://mediplus-1f427.firebaseio.com",
-  projectId: "mediplus-1f427",
-  storageBucket: "mediplus-1f427.appspot.com",
-  messagingSenderId: "1065801771983",
-  appId: "1:1065801771983:web:c0b4d908a9e67ee5bac0ba",
-  measurementId: "G-BVEF0GB2R2"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-
-
 
 export default function App() {
   let [fontsLoaded] = useFonts({
